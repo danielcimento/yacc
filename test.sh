e25ea9d5271c3c5c486a698684a96691e6c2268f
Daniel Cimento <daniel.cimento@mail.mcgill.ca>
2018-11-07 18:12:58 -0500

    Add addition and subtraction as features

--- test.sh
+++ test.sh
@@ -9,11 +9,15 @@ try() {
     actual="$?"
 
     if [ "$actual" != "$expected" ]; then
-        echo "$input expected, but got $actual"
+        echo "$expected expected for input $input, but got $actual"
         exit 1
     fi
 }
 
 try 0 0
 try 42 42
+try 12 4+8
+try 4 8-4
+try 21 5+20-4
+try 4 -1+5
 echo OK
--- yacc.c
+++ yacc.c
@@ -7,10 +7,37 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
+    // Preliminary Headers
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("\tmov rax, %d\n", atoi(argv[1]));
+    
+    // Read out the first character as a 10 bit number
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        // Handle addition instructions
+        if (*p == '+') {
+            p++;
+            // Note: we don't need p++ afterwards, as strtol will update p 
+            // with the start of the string after the number
+            printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        // Handle subtraction instructions
+        if(*p == '-') {
+            p++;
+            printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character: '%c' (Character code: %d)\n", *p, *p);
+        return 1;
+    }
+
     printf("\tret\n");
     return 0;
 }
